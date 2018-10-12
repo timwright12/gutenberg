617e02b5bd05de07b26319b3394e9a7cc1b8ed88
Jorge <jorge.costa@developer.pt>
2018-10-12 19:47:17 +0100

    Used utils function in full-content

--- test/support/utils.js
+++ test/support/utils.js
@@ -6,6 +6,12 @@ import path from 'path';
 import { uniq } from 'lodash';
 
 export function getFileBaseNames( fixturesDir ) {
+	// We expect 4 different types of files for each fixture:
+	//  - fixture.html            : original content
+	//  - fixture.parsed.json     : parser output
+	//  - fixture.json            : blocks structure
+	//  - fixture.serialized.html : re-serialized content
+	// Get the "base" name for each fixture first.
 	return uniq(
 		fs.readdirSync( fixturesDir )
 			.filter( ( f ) => /(\.html|\.json)$/.test( f ) )
